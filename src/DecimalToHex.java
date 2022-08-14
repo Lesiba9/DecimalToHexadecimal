78aebbfff626d97249e076484f6ee510cbdca61a
Lesiba9 <78035582+Lesiba9@users.noreply.github.com>
2022-08-14 23:13:52 +0200

    Update DecimalToHex.java

--- src/DecimalToHex.java
+++ src/DecimalToHex.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class DecimalToHex {
+import java.util.Scanner;
+
+public class DecimalToHex {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner( System.in );
+        System.out.print("Enter a decimal number : ");
+        int num =sc.nextInt();
+
+        // For storing remainder
+        int remainder;
+
+        // For storing result
+        String result="";
+
+        // Digits in hexadecimal number system
+        char hex[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+
+        while(num > 0)
+        {
+            remainder = num % 16;
+            result = hex[remainder] + result;
+            num = num/16;
+        }
+        System.out.println("Decimal to hexadecimal: "+ result);
+    }
 }
